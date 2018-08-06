3ea15f7e5607aa71d181f1423c7554c56cc992d0
Laura Petre <laura.d.petre@gmail.com>
2018-08-06 18:10:42 +0100

    Create getWeather function to make the api call and pass it as props

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import Titles from "./components/Titles.js"
 import Form from "./components/Form.js"
 import Weather from "./components/Weather.js"
 
+require('dotenv').config()
 
+const URL = "http://api.openweathermap.org/data/2.5/weather?q="
+const API_KEY = "8b67121ad7a2705a42906e14a14c0859"
+const UNITS = "metric"
+const CITY = "London"
 
 class App extends React.Component {
+  getWeather = async () => {
+    const api_call = await fetch(
+      `${URL}${CITY},uk&appid=${API_KEY}&units=${UNITS}`
+    );
+
+    const data = await api_call.json();
+  }
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     )
