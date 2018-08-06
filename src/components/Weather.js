2754a809ea9408296042c3216f0990739fb7696c
Laura Petre <laura.d.petre@gmail.com>
2018-08-06 20:40:47 +0100

    Refactor weather component as function

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-class Weather extends React.Component {
-  render () {
+const Weather = (props) => {
     return (
       <div>
-        { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> }
-        { this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
-        { this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
-        { this.props.conditions && <p>Conditions: {this.props.conditions}</p> }
-        { this.props.description && <p>Description: {this.props.description}</p> }
-        { this.props.error && <p>{this.props.error}</p> }
+        { props.city && props.country && <p>Location: {props.city}, {props.country}</p> }
+        { props.temperature && <p>Temperature: {props.temperature}</p> }
+        { props.humidity && <p>Humidity: {props.humidity}</p> }
+        { props.conditions && <p>Conditions: {props.conditions}</p> }
+        { props.description && <p>Description: {props.description}</p> }
+        { props.error && <p>{props.error}</p> }
       </div>
-    )
-  }
+    );
 }
 
-export default Weather
+export default Weather;
