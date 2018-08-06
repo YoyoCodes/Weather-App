bb9ac838fe555dd64e0e7109a186cc5290783399
Laura Petre <laura.d.petre@gmail.com>
2018-08-06 20:01:58 +0100

    Add weather params to state

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ const API_KEY = "8b67121ad7a2705a42906e14a14c0859"
 const UNITS = "metric"
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    description: undefined,
+    humidity: undefined,
+    error: undefined
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
     const city=e.target.elements.city.value;
@@ -20,7 +29,15 @@ class App extends React.Component {
 
     const data = await api_call.json();
     console.log(data)
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      description: data.weather[0].description,
+      humidity: data.main.humidity
+    })
   }
+
   render() {
     return (
       <div>
