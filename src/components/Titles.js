622aadf3075444e08ce6deed77bf255bd3f238bb
Laura Petre <laura.d.petre@gmail.com>
2018-08-06 20:45:18 +0100

    Refactor titles as function

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,12 @@
 import React from "react";
 
-class Titles extends React.Component {
-  render() {
+const Titles = () =>  {
     return (
       <div>
         <h1>Weather Now</h1>
         <p>Find out the temperatute, conditions and more ...</p>
       </div>
     )
-  }
 }
 
 export default Titles
